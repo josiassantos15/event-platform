8a0f72a48e8eb8d125f04bfeca47c0213a164c01
Josias Santos <josiassantos1577@gmail.com>
2022-07-03 22:45:35 -0300

    segunda parte

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,24 @@
+export function Lesson() {
+  return (
+    <a href="#" className="">
+      <span className="">
+        Terça • 15 de junho • 19h00
+      </span>
+
+      <div className="">
+        <header className="">
+          <span className="">
+            Conteúdo Liberado
+          </span>
+          <span className="">
+            AO VIVO
+          </span>
+        </header>
+
+        <strong className="">
+          Abertura do evento Ignite Lab
+        </strong>
+      </div>
+    </a>
+  )
+}
