ef35c7e478e5e2b988daa208ce8dfc278bd97d5b
Josias Santos <josiassantos1577@gmail.com>
2022-07-10 21:35:43 -0300

    Aula 02 - Fim

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -4,12 +4,12 @@ import { Video } from "../components/Video";
 
 export function Event () {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="flex flex-col min-h-screen">
+      <main className="flex flex-1">
         <Video />
         <Sidebar />
-      </div>
+      </main>
     </div>
   )
 }
