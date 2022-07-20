b0ae141e0616c85113b53248b26271e0161ac7bb
Josias Santos <josiassantos1577@gmail.com>
2022-07-19 22:40:23 -0300

    Aula 02- final

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -11,7 +11,7 @@ export function Event () {
       <Header />
       <main className="flex flex-1">
         { slug 
-          ? <Video /> 
+          ? <Video lessonSlug={slug}/> 
           : <div className="flex-1" /> 
         }
         <Sidebar />
